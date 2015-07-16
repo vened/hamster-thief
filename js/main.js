81dc9b1ea4c84fbc193d0fe4abf5068af244f6e6
Stolbukhin Maksim <maxstbn@gmail.com>
2015-07-16 16:00:19 +0300

    video

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 $(function () {
 	$.stellar({
 		horizontalScrolling: false,
-		verticalOffset     : 0
+		verticalOffset     : 0,
+		scrollProperty: 'scroll',
+		positionProperty: 'position',
+		parallaxElements: true
 	});
 });
