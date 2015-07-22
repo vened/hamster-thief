a7110add7fb854c50b98179dd020aff616330a37
Stolbukhin Maksim <maxstbn@gmail.com>
2015-07-22 18:45:53 +0300

    fixes

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@ $(function () {
 	$.stellar({
 		horizontalScrolling: false,
 		verticalOffset     : 0,
-		scrollProperty: 'scroll',
-		positionProperty: 'position',
-		parallaxElements: true
+		scrollProperty: 'scroll'
+		//scrollProperty: 'scroll',
+		//positionProperty: 'position'
+		//parallaxElements: true
 	});
 });
