91a0a78ba185da0edfe4a74d8a57f3cfe2dc382b
Stolbukhin Maksim <maxstbn@gmail.com>
2015-07-23 18:33:55 +0300

    fixes

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ $(function () {
 	$.stellar({
 		horizontalScrolling: false,
 		verticalOffset     : 0,
-		scrollProperty: 'scroll'
+		scrollProperty: 'scroll',
+		//scrollProperty: 'transform',
+		positionProperty: 'transform'
 		//scrollProperty: 'scroll',
 		//positionProperty: 'position'
 		//parallaxElements: true
